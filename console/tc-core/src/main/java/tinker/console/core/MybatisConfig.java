d5f42f462eaf2452d771e10c6cae0afee68d391f
佟亚鹏 <yapeng.tong@baidao.comc>
2016-10-31 14:07:54 +0800

    修复管理平台用户无法退出的问题

--- console/tc-core/src/main/java/tinker/console/core/MybatisConfig.java
+++ console/tc-core/src/main/java/tinker/console/core/MybatisConfig.java
@@ -47,8 +47,8 @@ public class MybatisConfig {
         dataSource.setUsername(userName);
         dataSource.setPassword(password);
  
-        dataSource.setInitialSize(5); // 连接池启动时创建的初始化连接数量（默认值为0）
-        dataSource.setMaxActive(20); // 连接池中可同时连接的最大的连接数
+        dataSource.setInitialSize(10); // 连接池启动时创建的初始化连接数量（默认值为0）
+        dataSource.setMaxActive(30); // 连接池中可同时连接的最大的连接数
         dataSource.setMaxIdle(12); // 连接池中最大的空闲的连接数，超过的空闲连接将被释放，如果设置为负数表示不限
         dataSource.setMinIdle(0); // 连接池中最小的空闲的连接数，低于这个数量会被创建新的连接
         dataSource.setMaxWait(60000); // 最大等待时间，当没有可用连接时，连接池等待连接释放的最大时间，超过该时间限制会抛出异常，如果设置-1表示无限等待
