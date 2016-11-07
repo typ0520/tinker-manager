136b8fa59ee7de39e4fc044ea41c6ca4b08b3810
jianjun.lin <jianjun.lin@baidao.com>
2016-11-07 16:58:02 +0800

    tmsdk-->patchsdk TinkerManager-->PatchManager

---
+++ tm-sdk/patchsdk/src/main/java/com/dx168/patchsdk/PatchManager.java
@@ -0,0 +1,279 @@
+package com.dx168.patchsdk;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.text.TextUtils;
+
+import com.dx168.patchsdk.bean.AppInfo;
+import com.dx168.patchsdk.bean.PatchInfo;
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.lib.util.TinkerLog;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by jianjun.lin on 2016/10/26.
+ */
+public final class PatchManager {
+
+    private static final String TAG = "PatchManager";
+
+    private static PatchManager instance;
+
+    public static PatchManager getInstance() {
+        if (instance == null) {
+            instance = new PatchManager();
+        }
+        return instance;
+    }
+
+    static void free() {
+        instance = null;
+        PatchServer.free();
+    }
+
+    public static final String SP_NAME = "patchsdk";
+    public static final String SP_KEY_USING_PATCH = "using_patch";
+
+    private Context context;
+    private String patchDirPath;
+    private AppInfo appInfo;
+    private String url;
+
+    public void init(Context context, String appId, String appSecret, String url) {
+        this.context = context;
+        if (!Tinker.with(context).isMainProcess()) {
+            return;
+        }
+        this.url = url;
+        appInfo = new AppInfo();
+        appInfo.setAppId(appId);
+        appInfo.setAppSecret(appSecret);
+        appInfo.setToken(PatchUtils.md5(appId + "_" + appSecret));
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            PackageInfo pkgInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            appInfo.setVersionName(pkgInfo.versionName);
+            appInfo.setVersionCode(pkgInfo.versionCode);
+            String appName = pkgInfo.applicationInfo.loadLabel(context.getPackageManager()).toString();
+            String hotFixDirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + appName + File.separator + "patchsdk";
+            patchDirPath = hotFixDirPath + File.separator + appInfo.getVersionName();
+            File hotFixDir = new File(hotFixDirPath);
+            if (hotFixDir.exists()) {
+                for (File patchDir : hotFixDir.listFiles()) {
+                    if (TextUtils.equals(appInfo.getVersionName(), patchDir.getName())) {
+                        continue;
+                    }
+                    patchDir.delete();
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setTag(String tag) {
+        if (!Tinker.with(context).isMainProcess()) {
+            return;
+        }
+        if (appInfo == null) {
+            throw new NullPointerException("PatchManager must be init before using");
+        }
+        appInfo.setTag(tag);
+    }
+
+    private PatchListener patchListener;
+
+    PatchListener getPatchListener() {
+        return patchListener;
+    }
+
+    public void queryAndApplyPatch() {
+        queryAndApplyPatch(null);
+    }
+
+    public void queryAndApplyPatch(final PatchListener patchListener) {
+        if (context == null) {
+            throw new NullPointerException("PatchManager must be init before using");
+        }
+        if (!Tinker.with(context).isMainProcess()) {
+            return;
+        }
+        this.patchListener = patchListener;
+        PatchServer.getInstance().get()
+                .queryPatch(url, appInfo.getAppId(), appInfo.getToken(), appInfo.getTag(),
+                        appInfo.getVersionName(), appInfo.getVersionCode(), appInfo.getPlatform(),
+                        appInfo.getOsVersion(), appInfo.getModel(), appInfo.getSdkVersion())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Subscriber<PatchInfo>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        if (patchListener != null) {
+                            patchListener.onQueryFailure(e);
+                        }
+                    }
+
+                    @Override
+                    public void onNext(final PatchInfo patchInfo) {
+                        if (patchInfo.getCode() != 200) {
+                            if (patchListener != null) {
+                                patchListener.onQueryFailure(new Exception("code=" + patchInfo.getCode()));
+                            }
+                            return;
+                        }
+                        if (patchListener != null) {
+                            patchListener.onQuerySuccess(patchInfo.toString());
+                        }
+                        if (patchInfo.getData() == null) {
+                            File patchDir = new File(patchDirPath);
+                            if (patchDir.exists()) {
+                                patchDir.delete();
+                            }
+                            TinkerInstaller.cleanPatch(context);
+                            if (patchListener != null) {
+                                patchListener.onCompleted();
+                            }
+                            return;
+                        }
+                        SharedPreferences sp = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
+                        String usingPatchPath = sp.getString(SP_KEY_USING_PATCH, "");
+                        String newPatchPath = getPatchPath(patchInfo.getData().getPatchVersion());
+                        if (TextUtils.equals(usingPatchPath, newPatchPath)) {
+                            if (patchListener != null) {
+                                patchListener.onCompleted();
+                            }
+                            return;
+                        }
+                        File patchDir = new File(patchDirPath);
+                        if (patchDir.exists()) {
+                            for (File patch : patchDir.listFiles()) {
+                                if (TextUtils.equals(patch.getName(), patchInfo.getData().getPatchVersion() + ".apk")) {
+                                    if (!checkPatch(patch, patchInfo.getData().getHash())) {
+                                        TinkerLog.e(TAG, "wrong hash");
+                                        if (patchListener != null) {
+                                            patchListener.onDownloadFailure(new Exception("wrong hash"));
+                                        }
+                                        return;
+                                    }
+                                    TinkerInstaller.cleanPatch(context);
+                                    TinkerInstaller.onReceiveUpgradePatch(context, patch.getAbsolutePath());
+                                    return;
+                                }
+                            }
+                        }
+                        downloadAndApplyPatch(newPatchPath, patchInfo.getData().getDownloadUrl(), patchInfo.getData().getHash());
+                    }
+
+                });
+
+    }
+
+    private void downloadAndApplyPatch(final String newPatchPath, String url, final String hash) {
+        PatchServer.getInstance().get()
+                .downloadFile(url)
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Subscriber<ResponseBody>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        if (patchListener != null) {
+                            patchListener.onDownloadFailure(e);
+                        }
+                    }
+
+                    @Override
+                    public void onNext(ResponseBody body) {
+                        byte[] bytes = null;
+                        try {
+                            bytes = body.bytes();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (!checkPatch(bytes, hash)) {
+                            TinkerLog.e(TAG, "wrong hash");
+                            if (patchListener != null) {
+                                patchListener.onDownloadFailure(new Exception("wrong hash"));
+                            }
+                            return;
+                        }
+                        PatchUtils.writeToDisk(bytes, newPatchPath);
+                        if (patchListener != null) {
+                            patchListener.onDownloadSuccess(newPatchPath);
+                        }
+                        TinkerInstaller.onReceiveUpgradePatch(context, newPatchPath);
+                    }
+                });
+    }
+
+    private boolean checkPatch(File patch, String hash) {
+        FileInputStream fis = null;
+        ByteArrayOutputStream bos = null;
+        byte[] bytes = null;
+        try {
+            fis = new FileInputStream(patch);
+            bos = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = fis.read(buf)) != -1) {
+                bos.write(buf, 0, len);
+            }
+            bytes = bos.toByteArray();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return checkPatch(bytes, hash);
+    }
+
+    private boolean checkPatch(byte[] bytes, String hash) {
+        if (bytes == null || bytes.length == 0) {
+            return false;
+        }
+        String downloadFileHash = PatchUtils.md5(appInfo.getAppId() + "_" + appInfo.getAppSecret() + "_" + PatchUtils.md5(bytes));
+        return TextUtils.equals(downloadFileHash, hash);
+    }
+
+    private String getPatchPath(String patchVersion) {
+        return patchDirPath + File.separator + patchVersion + ".apk";
+    }
+
+}
---
+++ tm-sdk/patchsdk/src/main/java/com/dx168/patchsdk/TinkerApplicationLike.java
@@ -0,0 +1,64 @@
+package com.dx168.patchsdk;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.multidex.MultiDex;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.dx168.patchsdk.tinker.SampleTinkerLog;
+import com.dx168.patchsdk.tinker.SampleTinkerManager;
+
+/**
+ * Created by jianjun.lin on 2016/10/25.
+ */
+public class TinkerApplicationLike extends DefaultApplicationLike {
+
+    public static Application application;
+
+    private static final String TAG = TinkerApplicationLike.class.getSimpleName();
+
+    public TinkerApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag,
+                                 long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent,
+                                 Resources[] resources, ClassLoader[] classLoader, AssetManager[] assetManager) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent, resources, classLoader, assetManager);
+    }
+
+    /**
+     * install multiDex before install tinker
+     * so we don't need to put the tinker lib classes in the main dex
+     *
+     * @param base
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+        SampleTinkerManager.setTinkerApplicationLike(this);
+        SampleTinkerManager.initFastCrashProtect();
+        //should set before tinker is installed
+        SampleTinkerManager.setUpgradeRetryEnable(true);
+
+        //optional set logIml, or you can use default debug log
+        TinkerInstaller.setLogIml(new SampleTinkerLog());
+
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        SampleTinkerManager.installTinker(this);
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+
+}
