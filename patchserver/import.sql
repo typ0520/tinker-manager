58b5f913f9a58a1e187b8965b7a6dee41112f76d
typ0520 <yapeng.tong@baidao.comc>
2017-02-10 12:01:25 +0800

    fix 1.1.0 sql

--- patchserver/import.sql
+++ patchserver/import.sql
@@ -9,9 +9,9 @@ v1.0.5
 alter table t_app_info add `package_name` varchar(64) DEFAULT NULL COMMENT 'android的包名 iOS的bundle_id'
 
 v1.1.0
-alter table t_app_info add `patch_size_jiagu` long COMMENT '给加固apk用的补丁的补丁大小'
-alter table t_app_info add `file_hash_jiagu` varchar(64) DEFAULT NULL COMMENT '给加固apk用的补丁的文件的hash值'
-alter table t_app_info add `download_url_jiagu` varchar(256) DEFAULT NULL COMMENT '给加固apk用的补丁的下载地址'
+alter table t_patch_info add `patch_size_jiagu` long COMMENT '给加固apk用的补丁的补丁大小'
+alter table t_patch_info add `file_hash_jiagu` varchar(64) DEFAULT NULL COMMENT '给加固apk用的补丁的文件的hash值'
+alter table t_patch_info add `download_url_jiagu` varchar(256) DEFAULT NULL COMMENT '给加固apk用的补丁的下载地址'
 */
 
 /*用户表*/
