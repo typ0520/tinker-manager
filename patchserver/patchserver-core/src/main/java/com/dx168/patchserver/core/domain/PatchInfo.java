863564e48618b120e09e2d359965b6a3572c0a2e
typ0520 <yapeng.tong@baidao.comc>
2017-02-13 11:38:02 +0800

    show jiagu patch

--- patchserver/patchserver-core/src/main/java/com/dx168/patchserver/core/domain/PatchInfo.java
+++ patchserver/patchserver-core/src/main/java/com/dx168/patchserver/core/domain/PatchInfo.java
@@ -247,6 +247,25 @@ public class PatchInfo {
         return fileSizeString;
     }
 
+    public String getFormatPatchSizeJiagu() {
+        long fileS = getPatchSizeJiagu();
+        if (fileS == 0) {
+            return "0k";
+        }
+        DecimalFormat df = new DecimalFormat("#.00");
+        String fileSizeString = "";
+        if (fileS < 1024) {
+            fileSizeString = df.format((double) fileS) + "B";
+        } else if (fileS < 1048576) {
+            fileSizeString = df.format((double) fileS / 1024) + "K";
+        } else if (fileS < 1073741824) {
+            fileSizeString = df.format((double) fileS / 1048576) + "M";
+        } else {
+            fileSizeString = df.format((double) fileS / 1073741824) + "G";
+        }
+        return fileSizeString;
+    }
+
     public String getFormatApplyScale() {
         if (applySize == 0) {
             return "0%";
